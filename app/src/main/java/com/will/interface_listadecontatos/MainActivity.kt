09166678b0506673a6df1ee80988a6a35c31a443
Willian Brandão <willian.ramos@aluno.cefet-rj.br>
2021-05-15 19:16:54 -0300

    declared the recycle view

--- app/src/main/java/com/will/interface_listadecontatos/MainActivity.kt
+++ app/src/main/java/com/will/interface_listadecontatos/MainActivity.kt
@@ -2,11 +2,23 @@ package com.will.interface_listadecontatos
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    //recycle view component
+    private  val rvList: RecyclerView by lazy {
+        findViewById<RecyclerView>(R.id.rv_list)
+    }
+    private val adapter = ContactAdapter()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bindViews()
     }
-    private fun
+    private fun bindViews(){
+
+    }
 }
