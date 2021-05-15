23b00022558f96079d6955f9cd841e7cabbe82d0
Willian Brandão <willian.ramos@aluno.cefet-rj.br>
2021-05-15 20:28:41 -0300

    create menu, and menu items

--- app/src/main/java/com/will/ContactMe/MainActivity.kt
+++ app/src/main/java/com/will/ContactMe/MainActivity.kt
@@ -2,6 +2,11 @@ package com.will.ContactMe
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Message
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -20,6 +25,8 @@ class MainActivity : AppCompatActivity() {
         bindViews()
         updateList()
     }
+
+
     private fun bindViews(){
         rvList.adapter = adapter
         rvList.layoutManager = LinearLayoutManager(this)
@@ -28,11 +35,54 @@ class MainActivity : AppCompatActivity() {
         adapter.updateList(
             arrayListOf(
                 Contact(
-                     "Willian Brandão",
-                     "(00) 0000-0000",
+                    "Silvester Stallone",
+                    "(00) 0000-0000",
+                    "img.png"
+                ),
+                Contact(
+                    "Arnold Schwarzenegger",
+                    "(00) 0000-0000",
+                    "img.png"
+                ),
+                Contact(
+                    "Chuck Norris",
+                    "(00) 0000-0000",
                     "img.png"
                 )
+
             )
         )
     }
+    private fun showToast(message: String){
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+    //create menu
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId){
+            R.id.item_menu1 -> {
+                showToast("Item 1")
+                return true
+            }
+            R.id.item_menu2 -> {
+                showToast("Item 2")
+                return true
+            }
+            R.id.item_menu3 -> {
+                showToast("Item 3")
+                return true
+            }
+            R.id.item_menu4 -> {
+                showToast("Item 4")
+                return true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
 }
